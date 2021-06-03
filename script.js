89b731ac63ab7c6abfb57359b8f618a3e2bb38ab
SamuelWebsterIs <>
2021-06-03 13:18:50 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,11 @@
+let side = 0
+let total = 0
+
+document.getElementById("button").addEventListener("click", math)
+
+function math () {
+  side = document.getElementById("leng").value
+  total = side*side
+  alert(total)
+  document.getElementById("leng").value = ''
+}
